77ac9998084bb435eac6d0bccbb17d2a51d91eac
Alsu <shakilsu@yandex.ru>
2022-11-25 20:09:45 +0300

    tasks

--- task27/Program.cs
+++ task27/Program.cs
@@ -2,3 +2,16 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int n = number;
+int result = 0;
+while(n > 0)
+{
+    result = result + n % 10;
+    n = n / 10;        
+}
+Console.WriteLine(result);
--- task29/Program.cs
+++ task29/Program.cs
@@ -1,23 +1,36 @@
-﻿using static Library;
-//  Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
-//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-//6, 1, 33 -> [6, 1, 33]
+﻿//  Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
 
-// метод, который создаёт массив
-// печать массива
+int[] CreateArray(int array)
+{
+    return new int[array];
+}
+
+void Fill(int[] array, int min, int max)
+{ 
+    int size = array.Length; 
+    for (int i = 0; i < size; i++)
+    {
+        //array[i] = Random.Shared.Next(min, max + 1);
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+string PrintGood(int[] numbers)
+    {
+        int size = numbers.Length;
+        int i = 0;
+        string result = "[ ";
+
+        while (i < size)
+        {
+            result += ($"{numbers[i],3} ");
+            i++;
+        }
+        return result + "]";
+    }
+
 
 int[] array = CreateArray(8);
 Console.WriteLine(PrintGood(array));
-
-// int[] array = {1, 2, 5, 7, 19, 6, 1, 33};
-
-// void PrintArray(int[] array)
-// {
-//     int count = array.Length;
-//     for(int i = 0; i < count; i++)
-//     {
-//         Console.Write ($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-// PrintArray(array);
+Fill(array, 1, 20); 
+Console.WriteLine(PrintGood(array)); 
